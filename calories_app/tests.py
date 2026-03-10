7f8f0e50f1dd53591409baffd596dcdd9fcdcf82
rithwik <rithwiksr2@gmail.com>
2026-03-10 09:39:20 +0000

    pylint

--- calories_app/tests.py
+++ calories_app/tests.py
@@ -1,9 +1,7 @@
-"""Django tests here"""
 from django.test import TestCase
 from django.urls import reverse
 
 class MyViewTestCase(TestCase):
-    """Djago test case"""
     def test_home_view(self):
         """Test if login page returns 200 status"""
         response = self.client.get(reverse('login'))
