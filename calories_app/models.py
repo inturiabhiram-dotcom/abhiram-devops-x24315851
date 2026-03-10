732c1b9dc63c05af2bad30ab05ffeb48a270dbec
rithwik <rithwiksr2@gmail.com>
2026-03-10 09:36:19 +0000

    pylint

--- calories_app/models.py
+++ calories_app/models.py
@@ -1,12 +1,10 @@
+"""Project Models here"""
 from django.db import models
 from django.contrib.auth.models import User
-
 from datetime import date
 
-# Create your models here.
-
-
 class Food(models.Model):
+	"""Food model"""
 	name = models.CharField(max_length=200 ,null=False)
 	quantity = models.PositiveIntegerField(null=False,default=0)
 	calorie = models.FloatField(null=False,default=0)
@@ -15,8 +13,8 @@ class Food(models.Model):
 	def __str__(self):
 		return self.name
 
-
 class Profile(models.Model):
+	"""Profile model"""
 	person_of = models.ForeignKey(User,null=True,on_delete=models.CASCADE)
 	calorie_count = models.FloatField(default=0,null=True,blank=True)
 	food_selected = models.ForeignKey(Food,on_delete=models.CASCADE,null=True,blank=True)
@@ -26,9 +24,6 @@ class Profile(models.Model):
 	calorie_goal = models.PositiveIntegerField(default=0)
 	all_food_selected_today = models.ManyToManyField(Food,through='PostFood',related_name='inventory')
 
-	
-
-	
 	def save(self, *args, **kwargs):# new
 		if self.food_selected != None:
 			self.amount = (self.food_selected.calorie/self.food_selected.quantity) 
@@ -38,18 +33,16 @@ class Profile(models.Model):
 			PostFood.objects.create(profile=calories_app,food=self.food_selected,calorie_amount=self.calorie_count,amount=self.quantity)
 			self.food_selected = None
 			super(Profile, self).save(*args,**kwargs)
-	
 		else:
 			super(Profile,self).save(*args,**kwargs)
-
-
 
 	def __str__(self):
 		return str(self.person_of.username)
 
-
 class PostFood(models.Model):
+	"""Postfood model"""
     profile = models.ForeignKey(Profile,on_delete=models.CASCADE)
     food = models.ForeignKey(Food,on_delete=models.CASCADE)
     calorie_amount = models.FloatField(default=0,null=True,blank=True)
     amount = models.FloatField(default=0)
+    
